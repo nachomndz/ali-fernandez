1dab983ae68ef761c3958f0405bd3ec76131c47f
MendezBonito, Ignacio <MendezBonito.Ignacio@bcg.com>
2024-09-04 19:43:03 +0200

    feature: images modified

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ali-fernandez/', 
 
   plugins: [react()],
+  base: '/ali-fernandez/', // Asegúrate de que esta línea esté correctamente configurada
+
 })
